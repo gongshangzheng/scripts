31f5c50d9defb921b8709482a1fe4b2906f9c5c2
Xinyu ZHENG <xinyuzheng31@gmail.com>
2025-01-26 20:56:22 +0100

    chore: Remove extra newline in git-repositories.sh and add Org repo clone

--- sys_config/git-repositories.sh
+++ sys_config/git-repositories.sh
@@ -10,7 +10,6 @@
 
 if [ ! -d "$HOME/.ssh" ]; then
     echo "ssh dir not exist, please generate ssh key first"
-
     exit 1
 fi
 
@@ -19,3 +18,4 @@ git clone git@github.com:gongshangzheng/my_vim.git ~/.vim_runtime
 git clone git@github.com:gongshangzheng/my_rime.git ~/.config/ibus/rime
 git clone git@github.com:gongshangzheng/emacs.git ~/.doom.d
 git clone git@github.com:gongshangzheng/gsai.git ~/blogs
+git clone git@github.com:gongshangzheng/Org.git ~/org
