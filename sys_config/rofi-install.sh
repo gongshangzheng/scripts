668a04a7b1db5c4215281fb1ff8eb757a0d0e67e
Xinyu ZHENG <xinyuzheng31@gmail.com>
2025-02-09 14:31:14 +0100

    rofi 支持中文

--- sys_config/rofi-install.sh
+++ sys_config/rofi-install.sh
@@ -8,13 +8,32 @@
 #   Description   ：
 # ================================================================
 
-sudo apt install rofi
+sudo apt install -y git meson ninja-build pkg-config libpango1.0-dev libcairo2-dev libxkbcommon-dev libxcb-util-dev libxcb-xinerama0-dev libxcb-imdkit-dev libxcb-xkb-dev libxkbcommon-x11-dev libxcb-ewmh-dev \
+    libxcb-icccm4-dev libstartup-notification0-dev flex bison
 
-if [ $APPLICATION_DIR -ne 0 ]; then
-    mkdir -p $APPLICATION_DIR
+
+ # Ask about Chinese support
+read -p "Do you need Chinese support? [y/N] " answer
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+     # Create application directory if it doesn't exist
+    APPLICATION_DIR="${APPLICATION_DIR:-$HOME/applications}"
+    mkdir -p "$APPLICATION_DIR"
+    # Clone and build rofi with Chinese support
+    cd "$APPLICATION_DIR"
+    git clone https://github.com/davatorium/rofi.git
+    cd rofi
+    git submodule update --init
+    meson setup build
+    ninja -C build
+    sudo ninja -C build install
+else
+    sudo apt install -y rofi
 fi
 
-git clone https://github.com/lr-tech/rofi-themes-collection.git $APPLICATION_DIR/rofi-themes-collection
-cd $APPLICATION_DIR/rofi-themes-collection
-mkdir -p ~/.local/share/rofi/themes
-cp themes/* ~/.local/share/rofi/themes
+# 克隆主题并复制
+if ! [[ -e $APPLICATION_DIR/rofi-themes-collection ]]; then
+    git clone https://github.com/lr-tech/rofi-themes-collection.git $APPLICATION_DIR/rofi-themes-collection || exit 1
+    cd $APPLICATION_DIR/rofi-themes-collection || exit 1
+    mkdir -p ~/.local/share/rofi/themes
+    cp themes/* ~/.local/share/rofi/themes || exit 1
+fi
